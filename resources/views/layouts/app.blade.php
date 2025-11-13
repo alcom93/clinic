e51daa585806df859dade04a07de48756dc84d0b
alcom93 <georgeshabibou@yahoo.fr>
2025-11-13 20:24:05 +0000

    ew

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,13 @@
-<html><head>
-<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-</head><body>
-<div class="container mx-auto p-4">
-@yield('content')
-</div>
-</body></html>
+<html>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container mx-auto p-4">
+        @yield('content')
+    </div>
+</body>
+
+</html>
